16e44f8fe5ffbbd441173761b63eaef8f35d6c6a
Jamie Winsor <jamie@enmasse.com>
2011-07-06 14:35:02 -0700

    Initial public release of x264 cookbook

---
+++ recipes/source.rb
@@ -0,0 +1,45 @@
+#
+# Cookbook Name:: x264
+# Recipe:: source
+#
+# Author:: Jamie Winsor (<jamie@enmasse.com>)
+#
+# Copyright 2011, En Masse Entertainment, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include_recipe "build-essential"
+include_recipe "git"
+
+x264_packages.each do |pkg|
+  package pkg do
+    action :purge
+  end
+end
+
+git "#{Chef::Config[:file_cache_path]}/x264" do
+  repository node[:x264][:git_repository]
+  reference node[:x264][:git_revision]
+  action :sync
+  notifies :run, "bash[compile_x264]"
+end
+
+bash "compile_x264" do
+  cwd "#{Chef::Config[:file_cache_path]}/x264"
+  code <<-EOH
+    ./configure --enable-static
+    make clean && make && make install
+  EOH
+  action :nothing
+end
