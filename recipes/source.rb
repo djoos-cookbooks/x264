c7c729cb5520880993caf8c6faa9e09711dc1ec2
Jamie Winsor <jamie@enmasse.com>
2011-07-06 23:52:08 -0700

    Fix missing package prerequisite installation - yasm

--- recipes/source.rb
+++ recipes/source.rb
@@ -28,6 +28,15 @@ x264_packages.each do |pkg|
   end
 end
 
+yasm_package = value_for_platform(
+  [ "ubuntu" ] => { "default" => "yasm" },
+  "default" => "yasm"
+)
+
+package yasm_package do
+  action :upgrade
+end
+
 git "#{Chef::Config[:file_cache_path]}/x264" do
   repository node[:x264][:git_repository]
   reference node[:x264][:git_revision]
