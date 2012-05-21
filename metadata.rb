f3b747670a7126f41a2679751a49bb08817aa3fe
Jamie Winsor <jamie@vialstudios.com>
2012-05-21 11:57:08 -0700

    release 0.3.0

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,10 @@
+name             "x264"
 maintainer       "En Masse Entertainment"
 maintainer_email "jamie@vialstudios.com"
 license          "Apache 2.0"
 description      "Installs and configures the x264 video encoding library"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.0"
+version          "0.3.0"
 
 supports "ubuntu", "10.04"
 
