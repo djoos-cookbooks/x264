bd910cd1ed3270bf63cb99e5c390af755fb5b979
David Joos <david.joos@gmail.com>
2014-01-24 23:07:54 +0000

    version bump + addition of name attribute

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,10 @@
+name             "x264"
 maintainer       "David Joos"
 maintainer_email "development@davidjoos.com"
 license          "MIT"
 description      "Installs/Configures x264"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.4.1"
+version          "0.4.2"
 
 supports "ubuntu"
 
